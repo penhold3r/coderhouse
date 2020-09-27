4548078f3771e12c37dabc626310ad7fd8a7cfbd
penholder <penhold3r@gmail.com>
2020-09-27 05:55:13 -0300

    webpack copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,40 @@ const glob = require('glob')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const globSassImporter = require('node-sass-glob-importer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 // COMPONENTS
-const componentsFiles = glob.sync('./src/app/components/*/*.html')
-const components = componentsFiles.map(
-	file =>
-		new HtmlWebpackPlugin({
-			filename: file.replace(/\.\/src\/app\/components\/\w*/g, ''),
-			template: file,
-			inject: false,
-		})
-)
+// const componentsFiles = glob.sync('./src/app/components/*/*.html')
+// const components = componentsFiles.map(
+// 	file =>
+// 		new HtmlWebpackPlugin({
+// 			filename: file.replace(/\.\/src\/app\/components\/\w*/g, ''),
+// 			template: file,
+// 			inject: false,
+// 		})
+// )
 
-// INCLUDES
-const includeFiles = glob.sync('./src/app/includes/*.html')
-const includes = includeFiles.map(
-	file =>
-		new HtmlWebpackPlugin({
-			filename: file.replace(/\.\/src\/app\//g, ''),
-			template: file,
-			inject: false,
-		})
-)
+// // INCLUDES
+// const includeFiles = glob.sync('./src/app/includes/*.html')
+// const includes = includeFiles.map(
+// 	file =>
+// 		new HtmlWebpackPlugin({
+// 			filename: file.replace(/\.\/src\/app\//g, ''),
+// 			template: file,
+// 			inject: false,
+// 		})
+// )
 
-// VIEWS
-const viewsFiles = glob.sync('./src/app/views/*.html')
-const views = viewsFiles.map(
-	file =>
-		new HtmlWebpackPlugin({
-			filename: file.replace(/\.\/src\/app\//g, ''),
-			template: file,
-			inject: false,
-		})
-)
+// // VIEWS
+// const viewsFiles = glob.sync('./src/app/views/*.html')
+// const views = viewsFiles.map(
+// 	file =>
+// 		new HtmlWebpackPlugin({
+// 			filename: file.replace(/\.\/src\/app\//g, ''),
+// 			template: file,
+// 			inject: false,
+// 		})
+// )
 
 //
 module.exports = {
@@ -123,19 +122,30 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: 'css/style.css',
 		}),
-		//new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			filename: 'index.html',
 			template: './src/index.html',
 		}),
 		new CopyPlugin({
-			patterns: [{ from: path.join(__dirname, 'src/app/data'), to: 'data' }],
+			patterns: [
+				{
+					from: 'src/app/components/**',
+					to: '.',
+					flatten: true,
+					globOptions: {
+						ignore: ['**.js', '*.js', '*.component.js'],
+					},
+				},
+				{ from: path.join(__dirname, 'src/app/views'), to: 'views' },
+				{ from: path.join(__dirname, 'src/app/includes'), to: 'includes' },
+				{ from: path.join(__dirname, 'src/app/data'), to: 'data' },
+			],
 			options: {
 				concurrency: 100,
 			},
 		}),
-	]
-		.concat(components)
-		.concat(includes)
-		.concat(views),
+	],
+	// .concat(components)
+	// .concat(includes)
+	// .concat(views),
 }
